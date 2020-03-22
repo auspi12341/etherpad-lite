c3eed0b3f1b78b54710f90774076fc606cbaf366
John McLear <john@mclear.co.uk>
2020-03-22 14:23:17 +0000

    test again

--- tests/frontend/travis/remote_runner.js
+++ tests/frontend/travis/remote_runner.js
@@ -73,7 +73,7 @@ var sauceTestWorker = async.queue(function (testSettings, callback) {
 sauceTestWorker.push({
     'platform'       : 'Windows 10'
   , 'browserName'    : 'microsoftedge'
-  , 'version'        : '80.0'
+  , 'version'        : 'latest'
 });
 
 /*
